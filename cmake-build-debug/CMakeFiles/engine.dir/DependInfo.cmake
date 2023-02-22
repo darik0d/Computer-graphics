893189163375c3f2b12e6b55b5eb432337a57e11
darik0d <teleginaaleksandra1@gmail.com>
2023-02-22 14:13:41 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/dasha/Downloads/utils/easy_image.cc" "/mnt/c/Users/dasha/Downloads/utils/cmake-build-debug/CMakeFiles/engine.dir/easy_image.cc.o"
+  "/mnt/c/Users/dasha/Downloads/utils/engine.cc" "/mnt/c/Users/dasha/Downloads/utils/cmake-build-debug/CMakeFiles/engine.dir/engine.cc.o"
+  "/mnt/c/Users/dasha/Downloads/utils/ini_configuration.cc" "/mnt/c/Users/dasha/Downloads/utils/cmake-build-debug/CMakeFiles/engine.dir/ini_configuration.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
