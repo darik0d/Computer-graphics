d2a9c7dfed398e4bdf797372cd89e725fa23848e
darik0d <teleginaaleksandra1@gmail.com>
2023-02-24 14:40:02 +0100

    Les 2 (zonder stochast en juiste datastructuur)

--- engine.cc
+++ engine.cc
@@ -8,7 +8,11 @@
 #include <string>
 #include <list>
 #include <cmath>
-const double pi = 3.1415;
+const double pi = 3.141592653589793238462653589;
+img::Color vectorToColor(std::vector<double> kleur){
+    img::Color to_return = img::Color(kleur[0]*255, kleur[1]*255, kleur[2]*255);
+    return to_return;
+}
 class Color{
 public:
     Color(){};
@@ -73,7 +77,7 @@ std::pair<double,double> getMaximum(const Lines2D &lines){
     return to_return;
 }
 
-img::EasyImage draw2DLines(const Lines2D &lines, const int size){
+img::EasyImage draw2DLines(const Lines2D &lines, const int size, img::Color background_color, img::Color line_color){
     // Bereken x_min, x_max, y_min, y_max;
     double x_min = getMinimum(lines).first;
     double y_min = getMinimum(lines).second;
@@ -96,30 +100,31 @@ img::EasyImage draw2DLines(const Lines2D &lines, const int size){
     double dx = imagex/2 - dcx;
     //dy
     double dy = imagey/2 - dcy;
-    img::EasyImage to_return(imagex, imagey);
+    img::EasyImage to_return(imagex, imagey, background_color);
     for(auto lijn: lines){
         // Vermenigvuldig alle punten met d
         lijn.a.x = std::round(d*lijn.a.x + dx);
         lijn.a.y = std::round(d*lijn.a.y + dy);
         lijn.b.x = std::round(d*lijn.b.x + dx);
         lijn.b.y = std::round(d*lijn.b.y + dy);
-        img::Color trans_color = img::Color(100, 100, 100);
-        to_return.draw_line(lijn.a.x, lijn.a.y, lijn.b.x, lijn.b.y, trans_color);
+        to_return.draw_line(lijn.a.x, lijn.a.y, lijn.b.x, lijn.b.y, line_color);
     }
     return to_return;
 }
 Lines2D drawLSystem(const LParser::LSystem2D &l_system){
+    std::vector<std::pair<double,double>> positie_stack;
+    std::vector<double> hoek_stack;
     std::set<char> alfabet = l_system.get_alphabet();
     // Gehardcoded kleur
     Color kleur = Color(150,150,150);
     // Gehardcoded grootte van de stap
-    int stap_grootte = 30;
+    double stap_grootte = 100.0;
     Lines2D to_return;
     // Get oorspronkelijke string
     std::string begin = l_system.get_initiator();
     // String to_return
-    std::string res = begin;
-    // Loop alle strings over (char per char)
+    std::string res = "";
+    // Loop alle strings over (char per char) om tot finale string te bekomen
     int iterations = l_system.get_nr_iterations();
     while (iterations > 0){
         for(char sym: begin){
@@ -133,13 +138,13 @@ Lines2D drawLSystem(const LParser::LSystem2D &l_system){
     // Set oorspronkelijke parameters
     double huidige_hoek = l_system.get_starting_angle()*pi/180;
     double hoek = l_system.get_angle()*pi/180;
-    std::pair<int,int> positie = std::make_pair(0,0);
+    std::pair<double,double> positie = std::make_pair(0,0);
     for(char sym:begin){
         // Als symbool in alfabet zit:
         if(alfabet.find(sym) != alfabet.end()){
             // Voor elke stap voeg een nieuwe lijn toe aan de Lines 2D
             std::pair<double,double> co1 = positie;
-            std::pair<double,double> co2 = std::make_pair(positie.first + stap_grootte*std::cos(huidige_hoek), positie.second + std::sin(huidige_hoek));
+            std::pair<double,double> co2 = std::make_pair(positie.first + stap_grootte*std::cos(huidige_hoek), positie.second + stap_grootte*std::sin(huidige_hoek));
             Point2D p1 = Point2D(co1);
             Point2D p2 = Point2D(co2);
             Line2D lijn = Line2D(p1,p2,kleur);
@@ -152,6 +157,16 @@ Lines2D drawLSystem(const LParser::LSystem2D &l_system){
         else{
             if(sym == '+') huidige_hoek += hoek;
             else if(sym == '-') huidige_hoek -= hoek;
+            else if(sym == '(') {
+                positie_stack.push_back(positie);
+                hoek_stack.push_back(huidige_hoek);
+            }
+            else if(sym == ')') {
+                positie = positie_stack[positie_stack.size()-1];
+                huidige_hoek = hoek_stack[hoek_stack.size()-1];
+                positie_stack.pop_back();
+                hoek_stack.pop_back();
+            }
             // In andere gevallen skip
         }
     }
@@ -159,27 +174,13 @@ Lines2D drawLSystem(const LParser::LSystem2D &l_system){
 }
 img::EasyImage generate_image(const ini::Configuration &configuration)
 {
-    // Deel 1
-    Point2D a = Point2D(0,0);
-    Point2D b = Point2D(80,40);
-    Color kleur = Color(150,150,150);
-    Line2D lijn1 = Line2D(a,b, kleur);
-    Point2D a2 = Point2D(0,0);
-    Point2D b2 = Point2D(40,80);
-    Color kleur2 = Color(150,150,150);
-    Line2D lijn2 = Line2D(a2,b2, kleur2);
-    Lines2D lijst = Lines2D();
-    lijst.push_back(lijn1);
-    lijst.push_back(lijn2);
-    // ???: Volgende lijnen in aparte functie steken (en alle andere dingen in de juiste klassen)
-    img::EasyImage to_return = draw2DLines(lijst, 30);
-
+    img::EasyImage to_return = img::EasyImage(200, 200, vectorToColor(configuration["General"]["backgroundcolor"]));
     LParser::LSystem2D l_systeem;
     std::ifstream input_stream(configuration["2DLSystem"]["inputfile"]);
     input_stream >> l_systeem;
     input_stream.close();
-    lijst = drawLSystem(l_systeem);
-    to_return = draw2DLines(lijst, 200);
+    Lines2D lijst = drawLSystem(l_systeem);
+    to_return = draw2DLines(lijst, configuration["General"]["size"], vectorToColor(configuration["General"]["backgroundcolor"]), vectorToColor(configuration["2DLSystem"]["color"]));
 	return to_return;
 }
 
